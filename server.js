3995786b677ff4c5bad942e0d8536ac5dd3cf60a
DannyBoyNYC <daniel.deverell@gmail.com>
2019-07-12 11:25:06 -0400

    done

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-mongodb+srv://daniel:dd2345@contactkeeper-qqnxe.mongodb.net/test?retryWrites=true&w=majority
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({ msg: 'yo!' });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Connected on port ${PORT}`));
+// mongodb+srv://daniel:dd2345@contactkeeper-qqnxe.mongodb.net/test?retryWrites=true&w=majority
